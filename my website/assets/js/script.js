12bd55396080fa5f4261917978b45152b90aac48
RicardoBirck <156687332+RicardoBirck@users.noreply.github.com>
2025-01-10 21:17:54 -0300

    More updates

---
+++ my website/assets/js/script.js
@@ -0,0 +1,34 @@
+const fitbodyButton = document.getElementById("fitbodyWebsiteButton");
+
+fitbodyButton.addEventListener("click", function() {
+    window.open("https://www.emilymeer.com/bodylura-app-dashboard", "_blank");
+  });
+
+
+const linguafrancaButton = document.getElementById("linguafrancaWebsiteButton");
+
+linguafrancaButton.addEventListener("click", function() {
+      window.open("https://www.emilymeer.com/lingua-franca-app", "_blank");
+    });
+
+
+const essentializeButton = document.getElementById("essentializeButton");
+
+essentializeButton.addEventListener("click", function() {
+    window.location.href = "/my website/pages/essentialize.html"; 
+});
+
+const essentializeButton2 = document.getElementById("essentializeButton2");
+
+essentializeButton2.addEventListener("click", function() {
+    window.location.href = "/my website/pages/essentialize.html"; 
+});
+
+
+document.getElementById("objectivismButton").addEventListener("click", function() {
+    window.location.href = "/my website/pages/objectivism.html"; 
+  });
+
+document.getElementById("objectivismButton2").addEventListener("click", function() {
+    window.location.href = "/my website/pages/objectivism.html"; 
+  });
